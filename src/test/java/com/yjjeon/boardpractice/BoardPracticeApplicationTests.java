33229fa3bca741a3219fbad0cdb5a8ee7199935b
jeon_yejin <yemong1226@gmaill.com>
2023-05-10 11:51:29 +0900

    #2 - 스프링부트 프로젝트 생성

---
+++ src/test/java/com/yjjeon/boardpractice/BoardPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjjeon.boardpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
